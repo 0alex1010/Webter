c7d5cb7c60350b80d9a6a365d69cf420758aaa0d
Alexer <robotalexjennings@gmail.com>
2024-10-24 18:25:59 -0500

    Update launch_mac.sh

--- launch_mac.sh
+++ launch_mac.sh
@@ -1,4 +1,22 @@
 clear
 mkdir webter
+echo "The installer has made a new folder: webter"
 cd webter
-curl -o 
+echo "Changed dir to: webter"
+echo "The installer will contune in 10 secs"
+sleep 10
+echo "Launched program: curl"
+curl -o webter.sh https://raw.githubusercontent.com/0alex1010/Webter/refs/heads/main/files/mac_file.sh
+echo "Launched shell script: webter.sh"
+bash webter.sh
+echo "End of script. Getting ready to exit..."
+echo "Launched program: rm"
+rm webter.sh
+echo "The following program has removed the file 'webter.sh': rm"
+cd ..
+echo "Changed dir to: .. (previus dir)"
+rmdir webter
+echo "Launched program: rmdir"
+echo "The following program has removed the folder 'webter': rmdir"
+clear
+
